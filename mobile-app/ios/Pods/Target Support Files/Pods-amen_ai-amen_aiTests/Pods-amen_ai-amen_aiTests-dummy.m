30c1e6c4cc248c93131cb2902e60b2d2f15dd915
Ayman <mcaymann@gmail.com>
2023-05-21 18:18:35 +0300

    remove big files

--- mobile-app/ios/Pods/Target Support Files/Pods-amen_ai-amen_aiTests/Pods-amen_ai-amen_aiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_amen_ai_amen_aiTests : NSObject
-@end
-@implementation PodsDummy_Pods_amen_ai_amen_aiTests
-@end
--- mobile-app/ios/Pods/Target Support Files/Pods-amen_ai/Pods-amen_ai-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_amen_ai : NSObject
-@end
-@implementation PodsDummy_Pods_amen_ai
-@end
